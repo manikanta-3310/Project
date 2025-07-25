e6f42a2b392362a9092e07fa49ffc1ca5631da3d
manikanta-3310 <manikantacherukupally@gmail.com>
2025-07-25 22:05:09 +0530

    Add files via upload

---
+++ SimpleCalculator.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
